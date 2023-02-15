9b6640ffce2e23f9f10c2425dff2b7669f71f26c
Marieli Ficagna <marieli.ficagna@gmail.com>
2023-02-15 19:47:28 -0300

    first commit

---
+++ src/testefinal/TesteFinal.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testefinal;
+
+/**
+ *
+ * @author Mari (48) 99618-5728
+ */
+public class TesteFinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 1; i < 5; i++) {
+	if (i==2) continue;
+	System.out.print("i = " + i); 
+}
+    }
+
+    
+}
